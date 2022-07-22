98759209eaf62ed24c346eba86e5e2194dd0c57c
mitibirru <357miteshbirru@gmail.com>
2022-07-22 13:47:30 +0530

    upto custom hooks for datafecthing

---
+++ src/pages/Board/Board.js
@@ -0,0 +1,32 @@
+import Lane from "../../components/Lane/Lane";
+import useDataFetching from "../../hooks/useDataFetching";
+import "./Board.css";
+
+const lanes = [
+  { id: 1, title: "To Do" },
+  { id: 2, title: "In Progress" },
+  { id: 3, title: "Review" },
+  { id: 4, title: "Done" },
+];
+
+function Board() {
+  const [loading, error, tasks] = useDataFetching(
+    `https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`
+  );
+
+  return (
+    <div className="Board-wrapper">
+      {lanes.map((lane) => (
+        <Lane
+          key={lane.id}
+          title={lane.title}
+          loading={loading}
+          error={error}
+          tasks={tasks.filter((task) => task.lane === lane.id)}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Board;
